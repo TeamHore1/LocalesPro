ed6063bb6e04dca40ade70916950cfb746eecb09
ilham99 <ilhamramdans573@gmail.com>
2026-06-04 14:50:44 +0700

    Delete backend directory

--- backend/api/ingredients/list.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once "../../config/cors.php";
-require_once "../../config/database.php";
-
-$stmt = $conn->query("SELECT * FROM ingredients");
-echo json_encode(["status" => "success", "data" => $stmt->fetchAll(PDO::FETCH_ASSOC)]);
-?>
